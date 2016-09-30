3d7e97adfa390ea463a906051f816d8478d950d1
Joshua Kelley <joshuakelley@Joshua-ZCW-17.local>
2016-09-30 16:43:25 -0400

    done

--- js/script.js
+++ js/script.js
@@ -1,28 +1,63 @@
 " use strict ";
 
-function PhoneBook(){
+var entries = [];
 
+function PhoneBook(){
 };
 
 function listAllNames() {
-  // code to list all names
+    display.innerHTML = null;
+    for (var i = 0;i < entries.length;i++){
+        display.innerHTML += entries[i].name + "<br>";
+    }
 };
 
 function listAllNumbers() {
-  // code to list all numbers
+    display.innerHTML = null;
+
+    for (var i = 0;i < entries.length;i++){
+        display.innerHTML += entries[i].number + "<br>";
+    }
 };
 
 function showAdd() {
   var name = prompt("Enter full name");
   var number = prompt("Enter phone number");
+    entries.push({name:name.toUpperCase(), number:number});
+    
 };
 
 function showRemove() {
-  var remove = prompt("Enter name to remove");
+    var remove = prompt("Enter name to remove");
+    var search = remove.toUpperCase();
+    for (var j = 0;j < entries.length;j++){
+        if(entries[j].name === search){
+        entries.splice(j,1);
+        break;
+        }
+    }
 };
 
 function showLookup() {
-  var lookup = prompt("Enter number to lookup");
+    display.innerHTML = null;
+    var lookupName = prompt("Enter name to lookup");
+    for (var e = 0;e < entries.length;e++){
+        if(entries[e].name == lookupName){
+            display.innerHTML += entries[e].name + " " + entries[e].number;break;
+        }
+    }
+};
+function reverseLookup() {
+    display.innerHTML = null;
+    var lookup = prompt("Enter number to lookup");
+    for (var o = 0;o < entries.length;o++){
+        if(entries[o].number == lookup){
+            display.innerHTML += entries[o].name + " " + entries[o].number;break;
+        }
+    }
 };
 
+function check (arrel){
+    return arrel
+}
 var display = document.getElementById("display");
